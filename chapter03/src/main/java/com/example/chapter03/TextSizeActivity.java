17e7f8dbb5e91f1ae96e52d46e3ab2ac7855300a
Chris <who7708@163.com>
2022-12-29 22:31:26 +0800

    text size

---
+++ chapter03/src/main/java/com/example/chapter03/TextSizeActivity.java
@@ -0,0 +1,17 @@
+package com.example.chapter03;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class TextSizeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_text_size);
+
+        // TextView tv_hello = findViewById(R.id.tv_hello);
+        // tv_hello.setTextSize(20);
+    }
+}
